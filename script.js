e116fc59af0a96020bf1141e1c2a6f1f1d41e067
Huiqi Zhou <blancazhou2011@gmail.com>
2017-03-16 21:18:04 -0400

    fixed the spinning bug

--- script.js
+++ script.js
@@ -42,64 +42,48 @@ $(document).ready(function(){
   front_page_circle = document.getElementsByClassName("front_page_background_img");
 
   function spin() {
-    TweenMax.from(".front_page_background_img",15,{rotation: 0})
     TweenMax.to(".front_page_background_img",15,{rotation: 15});
+    front_page_circle[0].removeEventListener("mouseenter", spin);
+    front_page_circle[0].addEventListener("mouseenter", spin_rev);
+  }
+  function spin_rev(){
+    TweenMax.to(".front_page_background_img",15,{rotation: -15});
+    front_page_circle[0].removeEventListener("mouseenter", spin_rev);
+    front_page_circle[0].addEventListener("mouseenter", spin);
   }
 
   front_page_circle[0].addEventListener("mouseenter", spin);
+
   //front page letter move
+  function letter_move(){
+    $(".front_page_wrapper").mouseenter(function() {
+        $(".h").addClass("hmove");
+        $(".u1").addClass("u1move");
+        $(".i1").addClass("i1move");
+        $(".q").addClass("qmove");
+        $(".i2").addClass("i2move");
+        $(".z").addClass("zmove");
+        $(".h2").addClass("h2move");
+        $(".o").addClass("omove");
+        $(".u2").addClass("u2move");
+        $(".web_developer_pic").addClass("web_developer_pic_move");
 
-
-      $(".front_page_wrapper").mouseenter(function() {
-          $(".h").addClass("hmove");
-          $(".u1").addClass("u1move");
-          $(".i1").addClass("i1move");
-          $(".q").addClass("qmove");
-          $(".i2").addClass("i2move");
-          $(".z").addClass("zmove");
-          $(".h2").addClass("h2move");
-          $(".o").addClass("omove");
-          $(".u2").addClass("u2move");
-          $(".web_developer_pic").addClass("web_developer_pic_move");
-
-          setTimeout(function(){
-            $(".h").addClass("hfinal");
-            $(".u1").addClass("u1final");
-            $(".i1").addClass("i1final");
-            $(".q").addClass("qfinal");
-            $(".i2").addClass("i2final");
-            $(".z").addClass("zfinal");
-            $(".h2").addClass("h2final");
-            $(".o").addClass("ofinal");
-            $(".u2").addClass("u2final");
-            $(".web_developer_pic").addClass("web_developer_pic_final");
-          }, 4000);
-      });
-
-
-
-  //line1, line2 pulling
-
-  line1 = document.getElementsByClassName("line1");
-  line2 = document.getElementsByClassName("line2");
-
-  function pull_right() {
-    TweenMax.from(this,0.25,{x: 120});
-    TweenMax.to(this,0.25,{x: 0});
-    TweenMax.from(this.nextElementSibling,0.25,{x: -120});
-    TweenMax.to(this.nextElementSibling,0.25,{x: 0});
+        setTimeout(function(){
+          $(".h").addClass("hfinal");
+          $(".u1").addClass("u1final");
+          $(".i1").addClass("i1final");
+          $(".q").addClass("qfinal");
+          $(".i2").addClass("i2final");
+          $(".z").addClass("zfinal");
+          $(".h2").addClass("h2final");
+          $(".o").addClass("ofinal");
+          $(".u2").addClass("u2final");
+          $(".web_developer_pic").addClass("web_developer_pic_final");
+        }, 4000);
+    });
   }
 
-  line1[0].addEventListener("mouseenter", pull_right);
-
-  function pull_left() {
-    TweenMax.from(this,0.25,{x: -120});
-    TweenMax.to(this,0.25,{x: 0});
-    TweenMax.from(this.previousElementSibling,0.25,{x: 120});
-    TweenMax.to(this.previousElementSibling,0.25,{x: 0});
-  }
-
-  line2[0].addEventListener("mouseenter", pull_left);
+  setTimeout(letter_move, 500)
 
   //project_div rotation
 
